d77dc608cb096e0ff190b0e7ab338c13cb4c3921
AnzhelikaEO <anzelikasht@gmail.com>
2019-10-27 14:33:22 +0300

    fix a bug, method sort count[] = new int[R+2]

--- JavaJune2017/src/homework/lesson170704/MSDint.java
+++ JavaJune2017/src/homework/lesson170704/MSDint.java
@@ -9,7 +9,6 @@ public class MSDint {
 	private static int[] aux;
 	
 	private static int digit (int number, int runk) {
-//		Integer.
 		if (runk > 9) {
 			System.out.println("Runk must be in range [0, 9]");
 			return -1;
@@ -17,8 +16,7 @@ public class MSDint {
 		int result = (int)(number/(Math.pow(10, runk)));
 		if (runk != 9) {
 			result %= 10;
-		}
-		
+		}		
 		return result;
 	}
 	
@@ -28,31 +26,37 @@ public class MSDint {
 		sort(a, 0, N-1, 9);
 	}
 	
-	private static void sort(int[] a, int lo, int  hi, int runk) {
+	private static void sort(int[] a, int lo, int  hi, int runk) {	
 		if(hi <= lo + M) {
 			insertionSort(a, lo, hi, runk);
-			return;
+			return; 
 		}
 		
-		int count[] = new int[R+1]; //вычисление счетчика повторений
+		int count[] = new int[R+2]; //вычисление счетчика повторений
 		for (int i = lo; i <= hi; i++) {
-			count[digit(a[i], runk) + 10]++;			
+			count[digit(a[i], runk) + 11]++;			
 		}
 		
+		count[0] = 0;
 		for (int i = 0; i < count.length - 1; i++) {//преобразование счетчиков в индексы
 			count[i+1] += count[i];
 		}
 		
 		for (int i = lo; i <= hi; i++) {//Распределение
-			aux[count[digit(a[i], runk) + 9]++] = a[i];
+			aux[count[digit(a[i], runk) +10]++] = a[i];
 		}
 
 		for (int i = lo; i <= hi; i++) {// Копирование назад
 			a[i] = aux[i - lo];
 		}
-		
 //		Рекурсивная сортировка для каждого значения символа
 		for (int r = 0; r < R ; r++) {
+//			int left = count[r];
+//			int right = count[r+1];
+//			if(right - left > 1)
+//			 {
+//				sort(a, lo + count[r], lo + count[r+1] - 1, runk - 1);
+//			}
 			sort(a, lo + count[r], lo + count[r+1] - 1, runk - 1);
 		}
 		
@@ -60,14 +64,14 @@ public class MSDint {
 	}
 	
 	private static void insertionSort(int[] a, int lo, int hi, int runk) {
-		//Сортировка от a[lo] до a[hi], начиная с d-го символа
+		//Сортировка от a[lo] до a[hi], начиная с runk разряда
 		for (int i = lo; i <= hi; i++) {
 			for (int j = i; j > lo && (a[j] % Math.pow(10, runk+1)) < (a[j-1] % Math.pow(10, runk+1)); j--) {
 				int x = a[j];
 				a[j] = a[j-1];
 				a[j-1] = x;
 			}
-		}		
+		}	
 	}
 	
 
@@ -82,14 +86,19 @@ public class MSDint {
 			copyMass[i] = mass[i];
 //			System.out.println(random.nextInt());
 		}
-		
-		
-		
-		int[] test = {2147483647, 1147483647, -2147483647, -1147483647,23,87,34,123};
-		sort(test);
-		for (int i = 0; i < test.length; i++) {
-			System.out.println(test[i]);
+		for(int i = 0; i < mass.length; i++) {
+			if(mass[i] != copyMass[i]) {
+				System.out.println("i = " + i + "\t" + mass[i] + "\t" + copyMass[i]);
+			}
 		}
+		
+		
+		
+//		int[] test = {2147483647, 1147483647, -2147483647, -1147483647,23,87,34,123};
+//		sort(test);
+//		for (int i = 0; i < test.length; i++) {
+//			System.out.println(test[i]);
+//		}
 		 long startTime;
 	     long stopTime;
 	     long elapsedTime;
@@ -104,9 +113,17 @@ public class MSDint {
 	     stopTime = System.currentTimeMillis();
 	     System.out.println("time Arrays.sort = " + (stopTime - startTime));
 	     
-	     for (int i = 0; i < mass.length; i += 70000) {
-			System.out.println(mass[i] + "\t" + copyMass[i]);
-		}
+//	     for (int i = 0; i < mass.length; i += 70000) {
+//			System.out.println(mass[i] + "\t" + copyMass[i]);
+//		}
+	     int count = 0;
+	     for (int i = 0; i < mass.length; i ++) {
+	    	 if (mass[i] != copyMass[i]) {	    		 
+	    		 System.err.println(mass[i] + "\t" + copyMass[i] + "\ti = " + i);
+	    		 count++;
+	    	 }
+	     }
+	     System.out.println("count errors = " + count);
 	     
 	     
 	}
