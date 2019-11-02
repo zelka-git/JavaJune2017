c9171c1cedb807d076eeb022cb7693ca1e6034a0
AnzhelikaEO <anzelikasht@gmail.com>
2019-11-02 13:24:49 +0300

    add lesson170717

---
+++ JavaJune2017/src/classwork/lesson170717/Harp.java
@@ -0,0 +1,5 @@
+package classwork.lesson170717;
+
+public class Harp extends PluckedStrings {
+
+}
---
+++ JavaJune2017/src/classwork/lesson170717/PluckedStrings.java
@@ -0,0 +1,5 @@
+package classwork.lesson170717;
+
+public abstract class PluckedStrings extends StringedInstruments {
+
+}
