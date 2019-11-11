fb3e8e9ce97c9367a6231de157f5cf5f99979137
AnzhelikaEO <anzelikasht@gmail.com>
2019-11-11 15:46:59 +0300

    add lesson170807

---
+++ JavaJune2017/src/classwork/lesson170807/ExseptionExample.java
@@ -0,0 +1,24 @@
+package classwork.lesson170807;
+
+public class ExseptionExample {
+
+	public static void main(String[] args) {
+		try {
+			m2();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		m();
+	}
+	
+	private static void m2() throws Exception{
+		
+	}
+
+	private static void m() throws RuntimeException{
+		
+	}
+	
+}
