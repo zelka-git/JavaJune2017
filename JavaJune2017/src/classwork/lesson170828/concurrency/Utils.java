0919c87a909135ffdf4eabdcd48cc72f8ee50dec
AnzhelikaEO <anzelikasht@gmail.com>
2019-11-24 01:12:42 +0300

    change -> public

--- JavaJune2017/src/classwork/lesson170828/concurrency/Utils.java
+++ JavaJune2017/src/classwork/lesson170828/concurrency/Utils.java
@@ -2,10 +2,10 @@ package classwork.lesson170828.concurrency;
 
 public class Utils {
 
-	static void pause() {
+	public static void pause() {
 		pause(1000);
 	}
-	static void pause(long millis) {
+	public static void pause(long millis) {
 		try {
 			Thread.sleep(millis);
 		} catch (InterruptedException e) {
